f9bae4b31f5781af3ce62cc96e1642f59dc00928
HHW101 <hyewonhwang@gmail.com>
2025-05-08 12:02:56 +0900

    test

---
+++ Assets/Monster/Scripts/EnemyController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Transform target;
+    [SerializeField] private float followRange=10f;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
