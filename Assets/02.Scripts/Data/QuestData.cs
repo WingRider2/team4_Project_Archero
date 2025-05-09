0a8a44fadf7eb0b9eaf315bab78b1dff71c0087a
MINSEOK SHIM <61370379+shimminseok@users.noreply.github.com>
2025-05-09 17:01:48 +0900

    feat : Quest 등록 함수 추가
    - Quest 등록 함수 추가

--- Assets/02.Scripts/Data/QuestData.cs
+++ Assets/02.Scripts/Data/QuestData.cs
@@ -3,34 +3,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[Serializable]
 public class QuestData
 {
+    [SerializeField] public string QuestName;
     public int ID;
-    public string    QuestName { get; private set; }
-    public QuestType QuestType { get; private set; }
-    public List<QuestCondition> Conditions; //퀘스트 조건 목록
-    public RewardData[] RewardDatas;
-    
-    
-    public bool IsCompleted()
-    {
-        return Conditions.TrueForAll(x => x.IsCompleted);
-    }
+    public QuestType QuestType;
+    public QuestCondition Condition; //퀘스트 조건 목록
+    public RewardData RewardData;
 }
 [Serializable]
 public class QuestCondition
 {
-    public QuestTargetType TargetType;
     public QuestConditionType ConditionType;
-    public int TargetID;
-    public int CurrentCount = 0;
     public int RequiredCount;
-    public bool IsCompleted => CurrentCount >= RequiredCount;
+    public QuestCondition Clone()
+    {
+        return (QuestCondition)this.MemberwiseClone();
+    }
 }
 
 [Serializable]
 public class RewardData
 {
-    public int[] RewardItemsList { get; private set; }
-    public int RewardGold { get; private set; }
+    public int[] RewardItemsList;
+    public int RewardGold;
+}
+
+
+public class SaveQuestData
+{
+    public int ID;
+    public int ClearCount;
+    public SaveQuestCondition Condition;
+    public bool IsComplete => Condition.IsComplete;
+    public SaveQuestData(QuestData questData)
+    {
+        ID = questData.ID;
+        Condition = new SaveQuestCondition(questData.Condition.Clone());
+    }
+}
+
+public class SaveQuestCondition
+{
+    public int CurrentCount;
+    public int RequiredCount;
+
+    public SaveQuestCondition(QuestCondition condition)
+    {
+        CurrentCount = 0;
+        RequiredCount = condition.RequiredCount;
+    }
+    public bool IsComplete => CurrentCount >= RequiredCount;
+
+    public void UpdateCount(int count)
+    {
+        CurrentCount += count;
+    }
 }
--- Assets/02.Scripts/Manager/QuestManager.cs
+++ Assets/02.Scripts/Manager/QuestManager.cs
@@ -1,64 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 
-public enum QuestTargetType
+public enum QuestType
 {
-    
+    Daily,
+    Weekly,
+    Achievement
 }
 
 public enum QuestConditionType
 {
-    
+    Challenge,
+    MonsterKill,
+    BossMonsterKill,
+    EquipmentLevelUp,
 }
 
-public enum QuestType
-{
-    
-}
 public class QuestManager : Singleton<QuestManager>
 {
-    
-    public Dictionary<(QuestTargetType, QuestConditionType), List<QuestCondition>> QuestConditionsMap
-    {
-        get;
-        private set;
-    } = new Dictionary<(QuestTargetType, QuestConditionType), List<QuestCondition>>();
-    
-    //현재 내가 수락한 실질적인 Quest List
-    public List<QuestData> CurrentAcceptQuestList { get; private set; } = new List<QuestData>();
-    public List<int>       ClearQuestList         { get; set; }         = new List<int>();
+    public List<SaveQuestData>                                      CurrentAcceptQuestList { get; private set; } = new List<SaveQuestData>();
+    public List<int>                                                ClearQuestList         { get; set; }         = new List<int>();
+    public Dictionary<QuestConditionType, List<SaveQuestCondition>> QuestConditionsMap     { get; private set; } = new Dictionary<QuestConditionType, List<SaveQuestCondition>>();
+
     void Start()
     {
-        
+        foreach (var questData in TableManager.Instance.GetTable<QuestTable>().DataDic.Values)
+        {
+            AcceptQuest(questData);
+        }
     }
 
-    void Update()
-    {
-        
-    }
-    /// <summary>
-    /// 퀘스트 수락 메서드
-    /// </summary>
-    /// <param name="quest"></param>
     public void AcceptQuest(QuestData quest)
     {
         if (CurrentAcceptQuestList.Exists(q => q.ID == quest.ID))
             return;
 
 
-        QuestData questData = quest;
-        for (int i = 0; i < quest.Conditions.Count; i++)
-        {
-            var condition = quest.Conditions[i];
-            if (!QuestConditionsMap.ContainsKey((condition.TargetType, condition.ConditionType)))
-            {
-                QuestConditionsMap[(condition.TargetType, condition.ConditionType)] = new List<QuestCondition>();
-            }
+        SaveQuestData questData = new SaveQuestData(quest);
 
-            QuestConditionsMap[(condition.TargetType, condition.ConditionType)].Add(condition);
+        SaveQuestCondition condition = questData.Condition;
+        if (!QuestConditionsMap.ContainsKey(quest.Condition.ConditionType))
+        {
+            QuestConditionsMap[quest.Condition.ConditionType] = new List<SaveQuestCondition>();
         }
-        CurrentAcceptQuestList.Add(quest);
+
+        QuestConditionsMap[quest.Condition.ConditionType].Add(condition);
+        CurrentAcceptQuestList.Add(questData);
+    }
+
+    public void UpdateCurrentCount(QuestConditionType type, int count)
+    {
+        if (QuestConditionsMap.TryGetValue(type, out List<SaveQuestCondition> conditions))
+        {
+            foreach (var condition in conditions)
+            {
+                condition.UpdateCount(count);
+            }
+        }
     }
 }
