71f232eb34bed4f7e743cb835b23411448882e2e
MINSEOK SHIM <61370379+shimminseok@users.noreply.github.com>
2025-05-09 13:02:37 +0900

    feat : Random Monster Spawn
    - Random Monster Spawn 추가

--- Assets/02.Scripts/Manager/MapManager.cs
+++ Assets/02.Scripts/Manager/MapManager.cs
@@ -11,6 +11,9 @@ public class MapManager : Singleton<MapManager>
     [SerializeField] private Tilemap wallMap;
     [SerializeField] private Tilemap colliderMap;
     [SerializeField] private GameObject[] obstacleObjects;
+
+    [SerializeField] private StageData[] stageDatas;
+    [SerializeField] private GameObject testPrefab;
     private void Awake()
     {
     }
@@ -19,6 +22,9 @@ public class MapManager : Singleton<MapManager>
         GenerateMap(TableManager.Instance.GetTable<StageTable>().GetDataByID(1));
     }
 
+    private void Update()
+    {
+    }
 
 
     public void GenerateMap(ChapterData chapterData)
@@ -49,13 +55,10 @@ public class MapManager : Singleton<MapManager>
     private void GenerateObstacle(StageData stageData)
     {
         List<Vector3Int> vaildPositions = GenerateSpawnArea();
-        
-
         if (vaildPositions.Count == 0)
             return;
 
         HashSet<int> tileIndex = new HashSet<int>();
-        //TODO : 스테이지에 있는 장애물 카운트를 가져와서 해줘야함.
         while (tileIndex.Count < stageData.ObstacleSpawnCount)
         {
             tileIndex.Add(Random.Range(0, vaildPositions.Count));
@@ -65,9 +68,12 @@ public class MapManager : Singleton<MapManager>
         {
             GameObject go = Instantiate(obstacleObjects[Random.Range(0, obstacleObjects.Length)]);
             go.transform.position = floorMap.CellToWorld(vaildPositions[index]) + new Vector3(0.5f,0.5f);
+            vaildPositions.RemoveAt(index);
         }
+
+        SpawnMonster(stageData, vaildPositions);
+
     }
-    
     List<Vector3Int> GenerateSpawnArea()
     {
         List<Vector3Int> result = new();
@@ -88,7 +94,20 @@ public class MapManager : Singleton<MapManager>
         return result;
     }
 
-
+    private void SpawnMonster(StageData stageData,List<Vector3Int> vaildPositions)
+    {
+        HashSet<int> tileIndex = new HashSet<int>();
+        while (tileIndex.Count < stageData.ObstacleSpawnCount)
+        {
+            tileIndex.Add(Random.Range(0, vaildPositions.Count));
+        }
+        
+        foreach (var index in tileIndex)
+        {
+            GameObject go = Instantiate(testPrefab);
+            go.transform.position = floorMap.CellToWorld(vaildPositions[index]) + new Vector3(0.5f,0.5f);
+        }
+    }
     private void OnDrawGizmosSelected()
     {
         if (floorMap == null || wallMap == null) return;
@@ -109,5 +128,7 @@ public class MapManager : Singleton<MapManager>
                 Gizmos.color = new Color(0f, 1f, 0f, 0.3f); // 연한 녹색
                 Gizmos.DrawCube(worldPos, new Vector3(1f, 1f, 0.1f));
             }
-        }    }
+        }    
+    }
+    
 }
