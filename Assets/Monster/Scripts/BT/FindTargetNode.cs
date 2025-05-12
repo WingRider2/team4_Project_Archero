228cc8600cb3a974ffdac72d6c65df48625fda4c
HHW101 <hyewonhwang@gmail.com>
2025-05-12 17:26:31 +0900

    Reapply "feat: 오류 수정"
    This reverts commit 7f848409b62afcdce3a2683f833d2bfb9d976413.

--- Assets/Monster/Scripts/BT/FindTargetNode.cs
+++ Assets/Monster/Scripts/BT/FindTargetNode.cs
@@ -12,19 +12,19 @@ public class FindTargetNode : INode
     }
     INode.ENodeState INode.Evaluate()
     {
-        Debug.Log("확인 실행중");
+    
         if (_enemy.Target == null)
             return INode.ENodeState.Failure;
         float distance = Vector2.Distance(_enemy.transform.position, _enemy.Target.position);
-        Debug.Log(distance);
+      
         if (distance <= _enemy.FindRange)
         {
-            Debug.Log("적 존재");
+
             return INode.ENodeState.Failure;
         }
         else
         {
-            Debug.Log("적 없음");
+      
             return INode.ENodeState.Success;
         }
     }
