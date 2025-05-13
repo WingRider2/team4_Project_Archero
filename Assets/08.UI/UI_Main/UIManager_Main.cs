44a04f931076f0510f9273f64c78aad2f78f8dc0
ybbro <uvinjjang@naver.com>
2025-05-13 12:06:44 +0900

    Feat: 전투 UI 스킬 아이콘 추가, 메인 UI 작업 중
    .

---
+++ Assets/08.UI/UI_Main/UIManager_Main.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class UIManager_Main : MonoBehaviour
+{
+    const string PlaySceneName = "SampleScene";
+    [SerializeField] Transform 
+        panel_StageSelect, // 스테이지 선택 버튼들이 있는 패널
+        panel_Setting; // 설정 메뉴가 있는 패널
+
+    // target으로 설정한 오브젝트 활성화/비활성화 전환
+    // 패널을 불러오는 버튼들에 할당
+    public void ChangeActive(GameObject target) => target.SetActive(!target.activeSelf);
+
+    // 나가기 버튼에 할당. 게임 종료 기능
+    public void GameQuit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false; // 에디터 플레이 모드 종료
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void SceneChange_Play()
+    {
+        SceneManager.LoadScene(PlaySceneName);
+    }
+}
