544db38683d0134a73aad5037122288277833911
HHW101 <hyewonhwang@gmail.com>
2025-05-14 17:54:33 +0900

    feat: 사운드 랜덤

--- Assets/02.Scripts/Sound/UnitSoundBase.cs
+++ Assets/02.Scripts/Sound/UnitSoundBase.cs
@@ -13,20 +13,32 @@ public enum UnitSoundType
 [CreateAssetMenu(menuName = "Audio/UnitSoundBase")]
 public class UnitSoundBase : ScriptableObject
 {
-    public AudioClip attack;
-    public AudioClip hit;
-    public AudioClip die;
-    public AudioClip skill;
-    public AudioClip move;
+    public AudioClip[] attack;
+    public AudioClip[] hit;
+    public AudioClip[] die;
+    public AudioClip[] skill;
+    public AudioClip[] move;
     public AudioClip GetClip(UnitSoundType type)
     {
         return type switch
         {
-            UnitSoundType.Attack => attack,
-            UnitSoundType.Hit => hit,
-            UnitSoundType.Die => die,
-            UnitSoundType.Skill => skill,
-            UnitSoundType.Move => move,
+            UnitSoundType.Attack => attack[Random.Range(0, attack.Length)],
+            UnitSoundType.Hit => hit[Random.Range(0, hit.Length)],
+            UnitSoundType.Die => die[Random.Range(0, die.Length)],
+            UnitSoundType.Skill => skill[Random.Range(0,skill.Length)],
+            UnitSoundType.Move => move[Random.Range(0, move.Length)],
+            _ => null
+        };
+    }
+    public AudioClip GetClip(UnitSoundType type,int index)
+    {
+        return type switch
+        {
+            UnitSoundType.Attack => attack[index],
+            UnitSoundType.Hit => hit[index],
+            UnitSoundType.Die => die[index],
+            UnitSoundType.Skill => skill[index],
+            UnitSoundType.Move => move[index],
             _ => null
         };
     }
